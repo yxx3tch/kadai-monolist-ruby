dafe12d45b7908e3d5e2d0f4317ca13085b26c33
EC2 Default User <ec2-user@ip-172-31-35-155.us-east-2.compute.internal>
2018-02-09 05:25:40 +0000

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -10,10 +10,12 @@ class OwnershipsController < ApplicationController
       @item.save
     end
 
-    # Want 関係として保存
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
+    elsif params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'    
     end
 
     redirect_back(fallback_location: root_path)
@@ -25,6 +27,9 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
+    elsif params[:type] == 'Have'
+      current_user.unhave(@item) 
+      flash[:success] = '商品の Have を解除しました。'    
     end
 
     redirect_back(fallback_location: root_path)
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,7 @@ class Item < ApplicationRecord
   
   has_many :wants
   has_many :want_users, through: :wants, class_name: 'User', source: :user
+  
+  has_many :haves, class_name: 'Have'
+  has_many :have_users, through: :haves, class_name: 'User', source: :user
 end
